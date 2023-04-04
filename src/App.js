b819c6f57183a02a798daffad2747997df49df3a
Arun Sharma <arun.sharmahp89@gmail.com>
2023-04-04 12:53:46 +0530

    add router

--- src/App.js
+++ src/App.js
@@ -1,14 +1,40 @@
-import logo from './logo.svg';
+import React, { useEffect, useState } from "react";
+import { Routes, Route } from "react-router-dom"
 import './App.css';
 import JobSearch from './components/JobSearch';
 import Layput from './components/common-componets/Layput';
+import SingleJobPage from './components/SingleJobPage';
 
 function App() {
+  const [getJobs , setGetJobs]=useState([])  
+const [jobs, setJobs] = useState();
+const fetchData = async () =>  {
+
+  // const data = await  fetch("https://apis.camillerakoto.fr/fakejobs/jobs")
+  //     .then((response) => response.json())
+  //     .then((data) => setGetJobs(data));
+  
+  const response = await fetch('https://apis.camillerakoto.fr/fakejobs/jobs');
+  const data = await response.json();
+      setGetJobs(data)  
+      setJobs(data);
+
+}
+
+useEffect(() => {
+fetchData();
+},[])
   return (
 
+
 <Layput>
-<JobSearch/>
+<Routes>
+<Route path='/' element={<JobSearch getJobs={getJobs} setGetJobs={setGetJobs} jobs={jobs} setJobs={setJobs} />} />
+<Route path='/:id' element={<SingleJobPage getJobs={getJobs} jobs={jobs} setJobs={setJobs}/>} />
+<Route path="*" element={'PageNotFound'} />
+</Routes>
 </Layput>
+
   );
 }
 
